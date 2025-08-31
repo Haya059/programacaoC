462e30e548b51b102c29784c34ecb8813be3861d
Haya059 <156369251+Haya059@users.noreply.github.com>
2025-08-31 20:32:10 +0000

    Adicionado os printf das operações

--- operadores_matematicos.c
+++ operadores_matematicos.c
@@ -4,8 +4,27 @@ int main (){
     int numero1, numero2;
     int soma, subtracao, multiplicacao, divisao;
 
+
+    printf("Digite o número 1: \n");
+    scanf("%d", &numero1);
+    printf("Digite o número 2: \n");
+    scanf("%d", &numero2);
+
+    //operação soma
     soma = numero1 + numero2;
+
+    //operação subtração
     subtracao = numero1 - numero2;
+
+    //operação multiplicação
     multiplicacao = numero1 * numero2;
+
+    //operação divisão
     divisao = numero1 / numero2;
+
+
+    printf("A soma entre %d e %d é: %d\n", numero1, numero2, soma);
+    printf("A subtração entre %d e %d é: %d\n", numero1, numero2, subtracao);
+    printf("A multiplicação entre %d e %d é: %d\n", numero1, numero2, multiplicacao);
+    printf("A divisão entre %d e %d é: %d\n", numero1, numero2, divisao);
 }
