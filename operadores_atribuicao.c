ef8f8f2b3f53d2abfb799a6767fd6b2e08a38c1e
Haya059 <156369251+Haya059@users.noreply.github.com>
2025-08-31 20:46:55 +0000

    Ajustes com os da aula

--- operadores_atribuicao.c
+++ operadores_atribuicao.c
@@ -8,21 +8,23 @@ int main (){
     Atribuição de Multiplicação (*=)
     Atribuição de Divisão (/=)
     */
-    int numero1, numero2, resultado;
+    int numero1 = 10, numero2, resultado;
 
     resultado = 10;
     printf("Resultado: %d\n", resultado);
 
+    //resultado = resultado + 20;
     resultado += 20;
     printf("Resultado: %d\n", resultado);
 
-    resultado -= 5;
+    //resultado = resultado - numero1;
+    resultado -= numero1;
     printf("Resultado: %d\n", resultado);
 
-    resultado *= 2;
+    resultado *= 5;
     printf("Resultado: %d\n", resultado);
 
-    resultado /= 4;
+    resultado /= 2;
     printf("Resultado: %d\n", resultado);
 
     return 0;
