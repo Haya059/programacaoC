f92c3f88671491ad9d25f4a2d3ae75e337abf548
Haya059 <156369251+Haya059@users.noreply.github.com>
2025-08-31 20:47:31 +0000

    Ajustes com o exemplo da aula

--- operadores_atribuicao.c
+++ operadores_atribuicao.c
@@ -21,9 +21,11 @@ int main (){
     resultado -= numero1;
     printf("Resultado: %d\n", resultado);
 
+    //resultado = resultado * 5;
     resultado *= 5;
     printf("Resultado: %d\n", resultado);
 
+    //resultado = resultado / 2;
     resultado /= 2;
     printf("Resultado: %d\n", resultado);
 
